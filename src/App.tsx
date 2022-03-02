9913d607793ebc84ea476ef0726e0d58cedf8ada
Rakshit <rakshit087@gmail.com>
2022-03-02 23:33:13 +0530

    feat(global): added light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { CssBaseline, createTheme, ThemeProvider } from '@mui/material'
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      background: {
+        default: '#191919',
+        paper: '#242424',
+      }
+    },
+  })
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      background: {
+        default: '#FFFFFF',
+      }
+    },
+  })
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>
+        <p>Hello World</p>
+      </div>
+    </ThemeProvider>
   )
 }
 
