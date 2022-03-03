ac78014b4c45532009e484dd11dd54aa5d89d387
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-03-03 14:05:33 +0530

    Add path aliases and update viteconfig and tsconfig files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { Alias, defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+import * as tsconfig from './tsconfig.paths.json';
+
+function readAliasFromTsConfig(): Alias[] {
+  const pathReplaceRegex = new RegExp(/\/\*$/, '');
+  return Object.entries(tsconfig.compilerOptions.paths).reduce(
+    (aliases, [fromPaths, toPaths]) => {
+      const find = fromPaths.replace(pathReplaceRegex, '');
+      const toPath = toPaths[0].replace(pathReplaceRegex, '');
+      const replacement = path.resolve(__dirname, toPath);
+      aliases.push({ find, replacement });
+      return aliases;
+    },
+    [] as Alias[],
+  );
+}
+
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: readAliasFromTsConfig(),
+  },
+});
