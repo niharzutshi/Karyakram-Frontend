de9a3c3e577f4b9177280d2c26583929f7f559a6
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-03-03 14:21:47 +0530

    Add router and dashboard, landing page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { darkTheme } from '@src/theme';
+import { Dasboard } from '@pages/Dashboard/Dashboard';
+import { Home } from '@pages/Home/Home';
+
 
 export const App = () => {
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline />
-            <div>
-                <p>Hello World</p>
-            </div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Dasboard" element={<Dasboard />} />
+                </Routes>
+            </Router>
         </ThemeProvider>
     );
 };
---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Dasboard = () => {
+    return(
+        <div>
+            Dashboard
+        </div>
+    )
+}
