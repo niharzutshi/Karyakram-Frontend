b99ea0c3e9265812d4113a00b0bb7d6690a7c742
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-03-03 21:47:13 +0530

    Add recoil and UI theme toggle🌞🌚

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
-import { darkTheme } from '@src/theme';
 import { Dasboard } from '@pages/Dashboard/Dashboard';
 import { Home } from '@pages/Home/Home';
-
+import { darkThemeSelectedState } from './store/DarkThemeSelectedState';
+import { darkTheme, lightTheme } from './theme';
 
 export const App = () => {
+    const darkThemeSelected = useRecoilValue(darkThemeSelectedState);
     return (
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme = { darkThemeSelected ? darkTheme : lightTheme } >
             <CssBaseline />
             <Router>
                 <Routes>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DarkModeToggle from "react-dark-mode-toggle";
+import { useRecoilState } from 'recoil';
+
+import { darkThemeSelectedState } from '@src/store/DarkThemeSelectedState';
 
 export const Home = () => {
+    const [darkThemeSelected, setDarkThemeSelected] = useRecoilState(darkThemeSelectedState);
     return(
         <div>
             Landing Page
+            <DarkModeToggle
+                onChange={setDarkThemeSelected}
+                checked={darkThemeSelected}
+                size ={80}
+            />
         </div>
     )
 }
