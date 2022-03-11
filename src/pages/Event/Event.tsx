9514178be66df8ee10a14bf6816e8c3d4c370ac3
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-03-11 23:43:25 +0530

    Add a low effort dev navigation component 💻

---
+++ src/pages/Event/Event.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Event.scss';
+
+export const Event = () => {
+  return <div>Event Page</div>;
+};
---
+++ src/pages/User/User.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './User.scss';
+
+export const User = () => {
+  return <div>User Profile</div>;
+};
