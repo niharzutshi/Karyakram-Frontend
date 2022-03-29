dc7234580e51fa62fb839be16b51d933f030d6a8
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-03-29 14:19:36 +0530

    Fix search bar theme switch 🌚

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+import { useRecoilValue } from 'recoil';
 
+import { darkThemeSelectedState } from '../../store/DarkThemeSelectedState';
 import './SearchBar.scss';
 
 export const SearchBar = () => {
+  const darkThemeActive = useRecoilValue(darkThemeSelectedState);
   return (
-    <div className="search-bar__container">
-      <input type="text" placeholder="Search" className="search-bar__element" />
+    <div className={!darkThemeActive ? 'search-bar__container-dark' : 'search-bar__container'}>
+      <input
+        type="text"
+        placeholder="Search"
+        className={!darkThemeActive ? 'search-bar__element-dark' : 'search-bar__element'}
+      />
       <SearchIcon fontSize="large" className="search-bar__icon" />
     </div>
   );
