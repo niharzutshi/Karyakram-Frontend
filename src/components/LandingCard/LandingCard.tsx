c63ca6a2f46773365cf1f70228d954ddeaeaff72
ChiragSaxena5437 <saxenac7@gmail.com>
2022-04-01 00:34:48 +0530

    Added Grid page into a grid✨

--- src/components/LandingCard/LandingCard.tsx
+++ src/components/LandingCard/LandingCard.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import * as React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 import './LandingCard.scss';
-
+import { LandingInfo } from '@components/LandingInfo/LandingInfo';
 export const LandingCard = () => {
   return (
     <div className="landing-card__container">
-      <div className="landing-card__picture-container">
-        <img
-          className="landing-card__picture"
-          src="https://i.imgur.com/iqukoXx.png"
-          alt="trending-image"
-        />
-      </div>
-      <div className="landing-card__price">
-        0.1 <br /> ETH
-      </div>
-      <div className="landing-card__details">
-        West End London Conference <br /> -xyz abc
-        <br />
-        <button className="landing-card__button">Register Now</button>
-      </div>
+      <Grid container spacing={0} sx={{ marginLeft: '80px' }}>
+        <Grid xs={7}>
+          <LandingInfo />
+        </Grid>
+        
+        <Grid xs={4}>
+          <div className="landing-card__picture-container">
+            
+            <img
+              className="landing-card__picture"
+              src="https://i.imgur.com/iqukoXx.png"
+              alt="trending-image"
+            />
+          </div>
+          <div className="landing-card__price">
+            0.1 <br /> ETH
+          </div>
+          <div className="landing-card__details">
+            West End London Conference <br /> -xyz abc
+            <br />
+            <button className="landing-card__button">Register Now</button>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
