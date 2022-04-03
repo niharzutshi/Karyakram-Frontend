807bf180118d757996bdebd455ba557672c8b697
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-04-03 23:35:06 +0530

    Clean up the code for submission 🧹

--- src/components/LandingInfo/LandingInfo.tsx
+++ src/components/LandingInfo/LandingInfo.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+
+import { userAuthState } from '@src/store/UserAuthState';
 
 import './LandingInfo.scss';
 
 export const LandingInfo = () => {
+  const [userAuthenticated, setUserAuthenticated] = useRecoilState(userAuthState);
   return (
     <div className="landing-info__container">
       <div className="landing-info__text">
@@ -14,7 +18,9 @@ export const LandingInfo = () => {
         <br />
         Plan Meetings
       </div>
-      <button className="landing-info__button">Get Started</button>
+      <button className="landing-info__button" onClick={() => {
+          setUserAuthenticated(!userAuthenticated);
+        }}>Get Started</button>
       <img
         src="https://i.imgur.com/YuUnLZS.png"
         alt="astronaut2"
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
 
 import { LandingCard } from '@components/LandingCard/LandingCard';
-import { LandingInfo } from '@components/LandingInfo/LandingInfo';
-
-import { userAuthState } from '@src/store/UserAuthState';
 
 import './LandingPage.scss';
 
 export const LandingPage = () => {
-  const [userAuthenticated, setUserAuthenticated] = useRecoilState(userAuthState);
   return (
     <div className="landing__container">
-      <button
-        onClick={() => {
-          setUserAuthenticated(!userAuthenticated);
-        }}>
-        Authenticate
-      </button>
       <LandingCard />
     </div>
   );
