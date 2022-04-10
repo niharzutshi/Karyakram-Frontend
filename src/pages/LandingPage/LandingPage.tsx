c28009ce69a2253d2007a25163dc0de4702a5052
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-04-11 00:17:02 +0530

    Complete landing page UI implementation (desktop) 🎉

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 
 import { LandingCard } from '@components/LandingCard/LandingCard';
+import { LandingPageHeader } from '@components/LandingPageHeader/LandingPageHeader';
+import { LandingPageFooter} from '@components/LandingPageFooter/LandingPageFooter';
 
 import './LandingPage.scss';
 
 export const LandingPage = () => {
   return (
-    <div className="landing__container">
-      <LandingCard />
-    </div>
+    <Box sx={{ flexGrow: 1 }} className="landing__container">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <LandingPageHeader />
+        </Grid>
+        <Grid item xs={12}>
+          <LandingCard />
+        </Grid>
+        <Grid item xs={12}>
+          <LandingPageFooter />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
