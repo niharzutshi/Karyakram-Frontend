95b852ee22f4f1f3f07d970331008cc46c248f54
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-18 16:26:27 +0530

    Add event detail card for trending page highlight panel

---
+++ src/components/TrendingDisplayCard/TrendingDisplayCard.tsx
@@ -0,0 +1,72 @@
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+
+type TrendingDisplayCardProps = {};
+
+const TrendingDisplayCard: React.FC<TrendingDisplayCardProps> = () => {
+  return (
+    <Box
+      mt="auto"
+      position="relative"
+      display="block"
+      bgcolor="#2D2D2D"
+      width="180px"
+      margin="100px"
+      borderRadius={6}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "-95px",
+          textAlign: "center",
+          width: "100%",
+          zIndex: "2",
+        }}
+      >
+        <img
+          alt="user profile"
+          src="https://static.remove.bg/remove-bg-web/726c8211ef4fdb5ce44accdf843f9bab4d2a356a/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png"
+          style={{
+            width: "100px",
+            height: "100px",
+            display: "block",
+            borderRadius: "120px",
+            margin: "30px auto 0",
+            border: "4px solid #79507A",
+          }}
+        />
+      </div>
+      <Box
+        pt={6}
+        pb={3}
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+      >
+        <Typography variant="h5" style={{ cursor: "default " }}>
+          Event Name
+        </Typography>
+        <Typography color="gray" style={{ cursor: "default " }}>
+          Organization
+        </Typography>
+        <Typography color="white" style={{ cursor: "default " }}>
+          Date
+        </Typography>
+        <Button
+          style={{
+            marginTop: "15px",
+            backgroundColor: "#800C83",
+            borderRadius: 8,
+            padding: "10px 20px 10px 20px",
+            color: "white",
+            textTransform: "capitalize",
+          }}
+        >
+          Register
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default TrendingDisplayCard;
--- src/pages/Trending/Trending.tsx
+++ src/pages/Trending/Trending.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { TrendingCard } from "@components/TrendingCard/TrendingCard";
+import TrendingDisplayCard from "@components/TrendingDisplayCard/TrendingDisplayCard";
 
 export const Trending = () => {
   return (
     <div>
       Trending Events
       <TrendingCard />
+      <TrendingDisplayCard />
     </div>
   );
 };
