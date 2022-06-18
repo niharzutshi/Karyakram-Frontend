7aef9d5aa751817c6ff88bebc085e0540c81054a
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-18 23:43:33 +0530

    Add layout file with navbar

---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@components/Navbar/Navbar";
+import React, { PropsWithChildren } from "react";
+
+type HomeLayoutProps = {
+  children: any;
+};
+
+const HomeLayout: React.FC<PropsWithChildren<HomeLayoutProps>> = ({
+  children,
+}) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+
+export default HomeLayout;
