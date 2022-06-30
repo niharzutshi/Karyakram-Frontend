6983924f891ff76734e2ed5f31ef4ca64500832c
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-06-30 20:31:53 +0530

    Add conditional rendering to [user] page

--- src/pages/[user]/index.tsx
+++ src/pages/[user]/index.tsx
@@ -1,11 +1,40 @@
+import { Box } from "@mui/system";
+import { useRouter } from "next/router";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import HomeLayout from "../../components/Layouts/HomeLayout";
+import { auth } from "../../firebase/clientApp";
+import Calendar from "../calendar";
 
 const User = () => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
   return (
-    <HomeLayout>
-      <div>Test</div>
-    </HomeLayout>
+    <>
+      {user?.email?.split("@")[0] === router.query.user ? (
+        <HomeLayout>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "50px",
+            }}
+          >
+            To be handled by{"  "}
+            <a href="https://github.com/pulkitvyas08/Karyakram-Frontend/issues/84">
+              #84
+            </a>
+          </Box>
+        </HomeLayout>
+      ) : (
+        <Box>
+          In this calendar page we will pass props that will adjust the
+          components according to the page we want to render
+          <Calendar />
+        </Box>
+      )}
+    </>
   );
 };
 
